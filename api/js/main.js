fc6d891391fd3529c6c4c794d64c01d10f353e43
KevinZheng29 <113195602+KevinZheng29@users.noreply.github.com>
2023-01-06 14:01:40 -0500

    1

--- api/js/main.js
+++ api/js/main.js
@@ -1,3 +1,19 @@
 import "../styles/main.css";
 
-console.log("hi");
+/*function greet(name) {
+  const greetPromise = new Promise(function (resolve, reject) {
+    resolve(`Hello ${name}`);
+    reject(`error`);
+  });
+  return greetPromise;
+}
+
+const suzie = greet("suzie");
+
+console.log(suzie);
+
+const page = document.getElementById("hi");
+
+suzie.then((result) => {
+  page.insertAdjacentHTML("beforeend", `${result}`);
+}); */

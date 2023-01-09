8ab9bc6c846d7637647e88c8f4f8dfde39338bb4
KevinZheng29 <113195602+KevinZheng29@users.noreply.github.com>
2023-01-09 14:01:34 -0500

    1

--- api/js/main.js
+++ api/js/main.js
@@ -17,3 +17,20 @@ const page = document.getElementById("hi");
 suzie.then((result) => {
   page.insertAdjacentHTML("beforeend", `${result}`);
 }); */
+
+/*const URL = "https://api.quotable.io/random";
+
+async function getData(URL) {
+  const response = await fetch(URL);
+  console.log(response);
+}
+getData(URL); */
+
+const URL = "https://anime-facts-rest-api.herokuapp.com/api/v1";
+
+async function getData(URL) {
+  const response = await fetch(URL);
+  console.log(response);
+}
+
+getData(URL);

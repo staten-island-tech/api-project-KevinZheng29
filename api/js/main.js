aebde94dc7056aad447ddde89e875a5c9e204ff8
KevinZheng29 <113195602+KevinZheng29@users.noreply.github.com>
2023-01-11 15:59:03 -0500

    1

--- api/js/main.js
+++ api/js/main.js
@@ -26,9 +26,42 @@ async function getData(URL) {
 }
 getData(URL); */
 
-const page = document.getElementById("hi");
+const page = document.getElementById("page");
+const input = document.getElementById("input");
+const btn = document.getElementById("btn");
+/////////////////////////////////////////////////////
 
-const URL = "https://pokeapi.co/api/v2/pokemon/magikarp";
+btn.addEventListener("click", function () {
+  let userinput = input.value;
+  const url = `https://pokeapi.co/api/v2/pokemon/${userinput}`;
+  showresult(url);
+});
+
+async function showresult(url) {
+  try {
+    const response2 = await fetch(url);
+    const data2 = await response2.json();
+    const dataimage = data2.sprites;
+    if (response2.status > 199 || response2.status < 300) {
+      console.log(data2);
+      page.insertAdjacentHTML(
+        "beforeend",
+        `
+      
+      <div>
+      <img src="dataimage.back_default">
+      </div>
+      
+      `
+      );
+    } else {
+      console.log("Error");
+    }
+  } catch (error) {}
+}
+
+/*
+const URL = `https://pokeapi.co/api/v2/pokemon/charmander`;
 
 async function getData(URL) {
   const response = await fetch(URL);
@@ -36,4 +69,4 @@ async function getData(URL) {
   console.log(data);
 }
 
-getData(URL);
+getData(URL);*/
